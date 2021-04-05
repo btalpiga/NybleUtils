3eb83426d5ec9f41551c3dfd621615df6dd71c31
Bogdan <bogdan.talpiga@3nyble.com>
2021-04-05 10:46:10 +0300

    Adaugare equals/hashCode pt clasa Pair

--- src/main/java/com/nyble/util/Pair.java
+++ src/main/java/com/nyble/util/Pair.java
@@ -1,6 +1,7 @@
 package com.nyble.util;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Pair<U, T> implements Serializable {
     public U leftSide;
@@ -9,5 +10,19 @@ public class Pair<U, T> implements Serializable {
     public Pair(U leftSide, T rightSide) {
         this.leftSide = leftSide;
         this.rightSide = rightSide;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(leftSide, pair.leftSide) &&
+                Objects.equals(rightSide, pair.rightSide);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(leftSide, rightSide);
     }
 }
